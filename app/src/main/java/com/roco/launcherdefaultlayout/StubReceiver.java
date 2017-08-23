ea8f94b3b76847462c418ef771d4fc9be0b5bbb1
lele1415 <lele1415@163.com>
2017-08-23 12:26:41 +0800

    Add new project

---
+++ app/src/main/java/com/roco/launcherdefaultlayout/StubReceiver.java
@@ -0,0 +1,17 @@
+package com.roco.launcherdefaultlayout;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StubReceiver extends BroadcastReceiver {
+    public StubReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
